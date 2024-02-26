509174cdca97a00fab519afb3cc378eabccd1b40
Megha Chhapre <95691127+MeghaChhapre@users.noreply.github.com>
2024-02-27 01:48:01 +0530

    error ts next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    typescript:{
+    ignoreBuildErrors: true,
+  },
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
