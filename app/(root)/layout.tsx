d7852041948210c5151c362cb91bebb397ecc93f
Megha Chhapre <meghachhapre04@gmail.com>
2024-02-27 23:47:19 +0530

    darktheme

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,43 +2,46 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
-
-
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Threads',
   description: 'A Meta Threads Application'
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <Topbar />
-        <main className="flex flex-row">
-          <LeftSidebar />
-          <section className="main-container" >
-            <div className="w-full max-w-4xl">
-              {children}
-            </div>
-          </section>
-          <RightSidebar />
-        </main>
-        <Bottombar />
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
